1dc88b2b33a4733d3a85e779d91848fb4d1a3362
Daniel Reichelt <dr1400@web.de>
2024-08-09 17:32:03 +0200

    Added configuration dialog

--- VintageSymphony/Config/Configuration.cs
+++ VintageSymphony/Config/Configuration.cs
@@ -5,6 +5,7 @@ namespace VintageSymphony.Config;
 [JsonObject(MemberSerialization.Fields)]
 public class Configuration
 {
+    public bool InitialConfigurationShown = false;
     public float GlobalVolume = 1f;
     public bool LoadGameMusic = false;
     public bool LoadVintageSymphonyMusic = true;
